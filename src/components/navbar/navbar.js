7a4c210463e3ca7cb3e7a2193e0e7b3c6123b625
Jishnu N <38449704+jinkl13@users.noreply.github.com>
2025-05-22 14:13:21 +0530

    Update navbar.js
    added import link for react router dom

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import './navbar.css';
